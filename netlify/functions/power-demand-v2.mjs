99721c95c2d6644e924c3cbc1ef740af8c318c1f
Kushagra Agrawal <kushagra301001@gmail.com>
2026-09-05 01:28:14 +0530

    Remove broken duplicate power-demand-v2 function

--- netlify/functions/power-demand-v2.mjs
+++
@@ -1,5 +0,0 @@
-import { getStore } from "@netlify/blobs";
-import { fetchEIA, dataRows, latest } from "./eia.mjs";
-const SIGNALS=["gas_generation","total_load","wind_generation","solar_generation","generation_outages","load_forecast"];
-const store=()=>getStore("natgas-power-demand");
-async function state(){return (await store().get("state",{type:"json}))||{observations:[],usage:{},lastRun:null};}
